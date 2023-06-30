92ccaadf39205abc7e29361bcb6ea55e98f465e5
KURCVENK <KURCVENK@INLEN8520076843.corp.capgemini.com>
2023-06-30 12:34:20 +0530

    First Commit

---
+++ src/main/java/com/demo/in/SpringSecutityApp1Application.java
@@ -0,0 +1,13 @@
+package com.demo.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutityApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecutityApp1Application.class, args);
+	}
+
+}
